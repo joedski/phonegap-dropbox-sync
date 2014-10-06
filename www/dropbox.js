6e90c5e4b1962568a6087da06fd61cae4be0df07
Joseph Sikorski <joedski@gmail.com>
2014-10-06 11:55:21 -0400

    Abstracted call to Cordova.exec because I hate copy-pasting all that.

--- www/dropbox.js
+++ www/dropbox.js
@@ -9,11 +9,51 @@
 
 var dropbox = (function() {
     var pluginName = "DropboxPlugin";
+    var slice = [].slice;
 
-    var link = function( cb ) {
+    // callCordovaExec( commandName, cb )
+    // callCordovaExec( commandName, [ arg, arg, ... ], cb )
+    function callCordovaExec( commandName, args, cb ) {
+        if( ! cb && typeof args == 'function' ) {
+            cb = args;
+            args = [ '' ];
+        }
+
+        if( ! args ) {
+            args = [ '' ];
+        }
+        
+        // All examples I see put in at least an empty string for arguments
+        // if passing 0 arguments.
+        if( args.length === 0 ) {
+            args.push( '' );
+        }
+
+        if( typeof cb != 'function' ) {
+            throw new TypeError( 'Provided callback is not a function.  Arguments provided: ' + String( slice.call( arguments, 0 ) ) );
+        }
+
+        Cordova.exec(
+            function success( result ) {
+                cb( null, result );
+            },
+            function failure( error ) {
+                cb( error );
+            },
+            pluginName, commandName, args );
+    }
+
+    ////////////////////
+    // Account
+    ////////////////////
+
+    function link( cb ) {
+        // Manual because of setTimeout call in success.
         Cordova.exec(
             function( result ) {
                 // why timeout?
+                // The android version does this too.
+                // There must be some lag after the link is successful or something...
                 setTimeout( function() {
                     cb( null, result );
                 }, 1000);
@@ -24,72 +64,44 @@ var dropbox = (function() {
             pluginName, "link", [""]);
     }
 
-    var checkLink = function( cb ) {
-        Cordova.exec(
-            function( result ) {
-                cb( null, result );
-            },
-            function( error ) {
-                cb( error );
-            },
-            pluginName, "checkLink", [""]);
+    function checkLink( cb ) {
+        callCordovaExec( 'checkLink', cb );
     }
 
-    var unlink = function( cb ) {
-        Cordova.exec(
-            function( result ) {
-                cb( null, result );
-            },
-            function( error ) {
-                cb( error );
-            },
-            pluginName, "unlink", [""]);
+    function unlink( cb ) {
+        callCordovaExec( 'unlink', cb );
     }
 
-    var listFolder = function( path, cb ) {
-        Cordova.exec(
-            function( result ) {
-                cb( null, result );
-            },
-            function( error ) {
-                console.log( "getFiles error:" );
-                console.log( error );
-                cb( error );
-            },
-            pluginName, "listFolder", [path]);
+    ////////////////////
+    // File System
+    ////////////////////
+
+    function listFolder( path, cb ) {
+        callCordovaExec( 'listFolder', [ path ], cb );
     }
 
-    var addObserver = function( path, cb ) {
-        Cordova.exec(
-            function( result ) {
-                cb( null, result );
-            },
-            function( error ) {
-                cb( error );
-            },
-            pluginName, "addObserver", [path]);
+    function addObserver( path, cb ) {
+        callCordovaExec( 'addObserver', [ path ], cb );
     }
 
-    var readData = function( fileName, cb ) {
-        Cordova.exec(
-            function( result ) {
-                cb( null, result );
-            },
-            function( error ) {
-                cb( error );
-            },
-            pluginName, "readData", [fileName]);
+    function removeObserver( observerId, cb ) {
+        callCordovaExec( 'removeObserver', [ observerId ], cb );
     }
 
-    var readString = function( fileName, cb ) {
-        Cordova.exec(
-            function( result ) {
-                cb( null, result );
-            },
-            function( error ) {
-                cb( error );
-            },
-            pluginName, "readString", [fileName]);
+    ////////////////////
+    // Files and Data
+    ////////////////////
+
+    function readData( fileName, cb ) {
+        callCordovaExec( 'readData', [ fileName ], cb );
+    }
+
+    function readString( fileName, cb ) {
+        callCordovaExec( 'readString', [ fileName ], cb );
+    }
+
+    function getImageBase64String( fileName, cb ) {
+        callCordovaExec( 'getImageBase64String', [ fileName ], cb );
     }
 
     return {
